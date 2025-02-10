a51007cda52eef14836718664d60dc158fa82b4f
codewithdhruba01 <patidhrubaraj@gmail.com>
2025-02-10 11:03:04 +0530

    add practice question

---
+++ List_Method.py
@@ -0,0 +1,47 @@
+#List me hum Variable ko store kar sakte he
+"""""
+marks = [23, 34, 87, 98, 12, 75, 17]
+
+print(marks)
+
+#Type  bhi check kar sakta hu
+print(type(marks)) # class ---> list
+"""
+
+#------list--------
+"""
+student = ["Sumit", 25, "kolkata"] #list me update valu add
+student[1]="65" #update data 
+print(student) #print
+"""
+
+""" 
+list= [22, 45, 12, 55]
+list.append(30) #add new element at the end
+print(list)
+"""
+
+"""
+list= [22, 45, 12, 55]
+list.sort(reverse=True) #Sort in deseending order bara se chota
+print(list)
+"""
+
+"""
+list= [22, 45, 12, 55]
+list.sort() #Sort in Aseending order ----chota se bara
+print(list)
+"""
+
+"""
+list = ['a', 'b', 'c', 'd', 'e']
+list.reverse()  #reverse list last ka char pahale ayaga
+print(list)
+"""
+
+#insert Element at index
+"""
+list=[2, 1, 3]
+list.insert(1, 5) #iske matlab index 1 ke jaga pe 5 add hoga and baki sab aliment same rahega
+print(list)  #print hoga [2, 5, 1, 3]
+"""
---
+++ if_elif_else.py
@@ -0,0 +1,44 @@
+#If-elif-else------> Question Pactice
+
+#marks count System pass fail.
+"""
+marks = int (input("Enter your marks (1-100): "))
+if (marks >= 90):
+   print(" Grade O")
+elif(marks >= 80 and marks < 90):
+    print("Grate A")
+elif(marks >= 60 and marks < 80):
+    print("Grade B")
+elif(marks >= 40 and marks < 60):
+    print("Grade C")
+else:
+    print("fail")
+"""
+
+
+#odd Or Even Number.
+"""
+num=int (input("Enter the number : "))
+if(num % 2 == 0):
+    print("Even")
+else:
+    print("Odd")
+"""
+
+
+#Leargest of 5 Numbers
+
+a = int(input("Enter first Number: "))
+b = int(input("Enter Second Number: "))
+c = int(input("Enter Third Number: "))
+d = int(input("Enter forth Number: "))
+if(a >= b and a >= c and a >= d):
+   print("First Number Gratest", a)
+elif(b >= a and b >= c and b >= d):
+   print("Second Number Gratest", b)
+elif(c >= a and c >= b and c >= d):
+    print("third Number Gratest", c)
+else:
+    print("Forth Number Gratest", d)
+
+
