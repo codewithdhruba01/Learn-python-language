1089e507033272813a8fed5d6086c1b713dac185
codewithdhruba01 <patidhrubaraj@gmail.com>
2025-02-11 15:18:44 +0530

    add New practice Question

---
+++ Dictionary.py
@@ -0,0 +1,36 @@
+#Create a Dictionary
+"""
+info = {
+    "Name":"Dhrubaraj", #ya sab key valu he
+    "age": 22,
+    "is_Adult": True
+}
+print(info)
+"""
+
+#nested Dictionary
+#dictionary ke undar dict
+"""
+info={
+    "name": "Dhrubaraj Pati",
+     "subject" :{
+    "Bangali": 29,
+    "English": 54,
+    "math": 59,
+    "Bio": 79
+}
+}
+print(info)
+"""
+
+#Update a Dictionary
+info = {
+    "name":"Dhrubaraj",
+     "age": 22,
+    "is_Adult": True,
+}
+
+info["name"]= "Dhrubaraj Pati", #ya hoga update pati add hoga
+info["surname"]= "Pati" #isme ak key valu add hoga Surname pati
+
+print(info)
