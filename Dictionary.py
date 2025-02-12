605de47cecaf95aa676bd6b5040724d477ac9a43
codewithdhruba01 <patidhrubaraj@gmail.com>
2025-02-12 20:48:23 +0530

    update practice set question

--- Dictionary.py
+++ Dictionary.py
@@ -14,7 +14,7 @@ print(info)
 info={
     "name": "Dhrubaraj Pati",
      "subject" :{
-    "Bangali": 29,
+    "Bengali": 29,
     "English": 54,
     "math": 59,
     "Bio": 79
@@ -36,3 +36,91 @@ info["surname"]= "Pati" #isme ak key valu add hoga Surname pati
 
 print(info)
 """
+
+#indivisual Subject print
+"""
+info={
+    "name": "Dhrubaraj Pati",
+    "subject" : {
+      "Bengali": 29,
+      "English": 54,
+      "math": 59,
+      "Bio": 79
+}
+}
+print(info["subject"]) #isme keya huya ki info ke undar srip subject hi print huya
+
+print(info["subject"]["Bio"]) #spacific ak ko bhi print kar sakta he ase
+"""
+
+"""
+#Dictionary keys Methods
+info={
+    "name": "Dhrubaraj Pati",
+    "subject" : {
+      "Bengali": 29,
+      "English": 54,
+      "math": 59,
+      "Bio": 79
+}
+}
+print(info.keys()) #dict.keys() methods return all keys
+"""
+
+"""
+#Dictionary values Methods
+info={
+    "name": "Dhrubaraj Pati",
+    "subject" : {
+      "Bengali": 29,
+      "English": 54,
+      "math": 59,
+      "Bio": 79
+}
+}
+print(info.values()) #dict.values() methods return all values
+"""
+
+"""
+#Dictionary items Methods
+info={
+    "name": "Dhrubaraj Pati",
+    "subject" : {
+      "Bengali": 29,
+      "English": 54,
+      "math": 59,
+      "Bio": 79
+}
+}
+print(info.items()) #dict.items() methods return all (key values) pairs as tuples
+"""
+
+"""
+#Dictionary .get Methods
+info={
+    "name": "Dhrubaraj Pati",
+    "subject" : {
+      "Bengali": 29,
+      "English": 54,
+      "math": 59,
+      "Bio": 79
+}
+}
+print(info.get("name")) #return the key according to value
+"""
+
+
+#Dictionary update Methods
+info={
+    "name": "Dhrubaraj Pati",
+    "subject" : {
+      "Bengali": 29,
+      "English": 54,
+      "math": 59,
+      "Bio": 79
+}
+}
+info.update({"city": "kolkata"}) #update new key value in Dictionary
+print(info)
+
+
