72ed9465a93a9e20f80e0f76b010f097ea1153e8
codewithdhruba01 <patidhrubaraj@gmail.com>
2025-02-13 18:43:06 +0530

    add practice

---
+++ set_in_python.py
@@ -0,0 +1,16 @@
+#set is the collection of the unordered items.
+"""
+nums = {1,2,3,4,5,6} #this is a valid set
+print(nums)
+print(type(nums))
+"""
+
+"""
+nums2 = {1,2,3,3,3,"dhruba","dhruba"} #Duplicate velu not allow
+print(nums2)
+"""
+
+"""
+nums = set() #called Empty set
+print(type(nums))
+"""
