bbc2024a33f8a26903ea5ab64a6e6641ce6a87d1
codewithdhruba01 <patidhrubaraj@gmail.com>
2025-02-14 21:20:08 +0530

    add set lesson

--- set_in_python.py
+++ set_in_python.py
@@ -14,3 +14,19 @@ print(nums2)
 nums = set() #called Empty set
 print(type(nums))
 """
+#set add method --- set.add(element)
+#set element immutable.
+#duplicate velu not allow.
+"""
+example=set() #emty set
+example.add(34) #Add function matlab 34 velu set ke undar add ho geya
+example.add(56) #Add function
+example.add(56) #duplicate velu
+print(example) #print function
+"""
+#remove function in set-------
+fruits = {"apple", "banana", "cherry"} #this is a tuple.
+
+fruits.remove("banana") #remove function.
+print(fruits) #print function.
+print(type(fruits)) #Which type (set).
