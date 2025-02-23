3d8ee05a4791e9a4699852d798994a494e47b4d5
codewithdhruba01 <patidhrubaraj@gmail.com>
2025-02-23 18:48:37 +0530

    day-10 python learning

---
+++ loop.py
@@ -0,0 +1,18 @@
+#While Loop --
+"""
+while True: # matlab jabtak true is me koi bhi condition nahi he Srep true he
+    print("Hello World") #is ke matlab infinite loop hoga
+"""
+#question 1
+"""
+    count = int(input("Enter Start number: "))
+    x = int(input("Enter Stop number: "))
+    while count < x:
+        print(count)
+        count += 1  # This is the same as count = count + 1
+        """
+#While loop Example--
+x = 1
+while x <= 10: #Check karega x 10 se chota he ya nahi
+    print(x) #x ka velu print karega
+    x += 1 #Update method (X increse hoga 1 se)
