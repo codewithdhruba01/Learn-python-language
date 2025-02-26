a28605da9d31d1410e5776e4a5853d3e1be5a5df
codewithdhruba01 <patidhrubaraj@gmail.com>
2025-02-26 21:37:13 +0530

    learn loop

---
+++ Loop/for-loop.py
@@ -0,0 +1,13 @@
+#A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+#This is less like the for keyword in other programming languages, and works more like an iterator method as found
+#in other object-orientated programming languages.
+
+#hum for loop ko tupple, list ke undar stor kar sakte he
+
+friend = ["akash", "alock", "sumit"]
+for x in friend:
+  print(x)
+
+nums = [1, 3, 4, 7, 8]
+for i in nums: # nums ka  velu i ke undar stor hoge
+  print(i)
---
+++ Loop/range-functionForLoop.py
@@ -0,0 +1,14 @@
+#Range()---->
+#Range function return a sequence of numbers, string form ( '0' by default ) and increments by ( 1 by default)
+#And stop before a specified number.
+
+# range (Start?, Stop, Step?) ---> this is the range function
+"""
+for el in range (5): #matlab 0 - 4 tak hi chalega loop
+    print(el)
+
+for el in range (1,5): #matlab 1 - 4 tak hi chalega loop
+    print(el)
+    """
+for el in range (1,10,2): #matlab 0 - 4 tak hi chalega and har bar 2 + 1 increment hoga
+    print(el, end=" ") #end ke matlab ak line pe ayaga
