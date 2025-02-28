e6b19193bc0f5f7fe38082e8ed3e51a2432e0821
codewithdhruba01 <patidhrubaraj@gmail.com>
2025-02-28 10:52:09 +0530

    create design structure

--- Day-10_Loops/range-functionForLoop.py
+++ Day-10_Loops/range-functionForLoop.py
@@ -1,7 +1,3 @@
-#Range()---->
-#Range function return a sequence of numbers, string form ( '0' by default ) and increments by ( 1 by default)
-#And stop before a specified number.
-
 # range (Start?, Stop, Step?) ---> this is the range function
 """
 for el in range (5): #matlab 0 - 4 tak hi chalega loop
