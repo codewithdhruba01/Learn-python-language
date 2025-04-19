756d40524e692330e3d926c8de81f66bfd038f44
codewithdhruba01 <patidhrubaraj@gmail.com>
2025-04-19 10:47:12 +0530

    update string practice set

--- 04_String/String.py
+++ 04_String/String.py
@@ -16,7 +16,22 @@ str2 = "World"
 print(str1+str2) #Concatination 2 String
 
 #String Indexing
-nam = "Dhrubaraj" #0 1 2 3 4 5 6 7 8 this is a index number
+a = "Dhrubaraj" #0 1 2 3 4 5 6 7 8 - this is a index number. (always 0 Start index)
+print(a[4])
 #String me Space bhi count hota he
 
+#capitalize Function
+nam1= "Hello PYTHON" #Capitalize last Sentence
+out = nam1.capitalize()
+print(out) #Output - Hello python
 
+#Replace Function
+b = "I am Stadying Python"
+print(b) #I am Stadying Python
+print (b.replace("Python", "Java")) #I am Stadying Java
+
+#Slicing
+c = "Dhrubaraj Pati"
+print(c[10:14])
+print(c[5:])
+print(c[:14])
